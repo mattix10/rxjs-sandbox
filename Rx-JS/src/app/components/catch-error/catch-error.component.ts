be2998a6a690206f519717a03f205cbb9085fd60
mattix10 <mateusz.salamon10@gmail.com>
2022-04-25 09:27:55 +0200

    add catchError component

--- Rx-JS/src/app/components/catch-error/catch-error.component.ts
+++ Rx-JS/src/app/components/catch-error/catch-error.component.ts
@@ -11,17 +11,16 @@ export class CatchErrorComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    
-of(1, 2, 3, 4, 5).pipe(
-  map(n => {
-     if (n === 4) {
-       throw 'four!';
-    }
-   return n;
-  }),
-  catchError(err => of('I', 'II', 'III', 'IV', 'V')),
-)
-.subscribe(x => console.log(x));
+    of(1, 2, 3, 4, 5).pipe(
+      map(n => {
+        if (n === 4) {
+          throw 'four!';
+        }
+      return n;
+      }),
+      catchError(err => of('I', 'II', 'III', 'IV', 'V')),
+    )
+    .subscribe(x => console.log(x));
   }
 
 }
